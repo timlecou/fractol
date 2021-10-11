0397a343b381ca998c2465d722b095af25809e74
user42 <dkoriaki@student.42.fr>
2021-10-11 16:06:19 +0200

    refresh

--- srcs/launch.c
+++ srcs/launch.c
@@ -5,7 +5,7 @@ int	close_mlx_win(int keycode, t_mlx *mlx);
 int	launch_program(t_fractol *fractol)
 {
 	if (new_window(fractol->mlx, fractol->res))
-		return (print_error("an error has occured when creating a window\n", 1));
+		return (print_error("an error has occured when creating a window\n"));
 	mlx_hook(fractol->mlx->win, 2, 1L<<0, close_mlx_win, fractol->mlx);
 	mlx_loop(fractol->mlx->mlx);
 	return (0);
