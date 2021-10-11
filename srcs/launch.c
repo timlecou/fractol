6dfa6a32ae1c999a91690a8489dfe99993ab8a4b
timlecou <timlecou@student.42.fr>
2021-10-11 15:46:03 +0200

    added ESC key to close the window and added max values for the windows size

--- srcs/launch.c
+++ srcs/launch.c
@@ -1,9 +1,12 @@
 #include "fractol.h"
 
+int	close_mlx_win(int keycode, t_mlx *mlx);
+
 int	launch_program(t_fractol *fractol)
 {
 	if (new_window(fractol->mlx, fractol->res))
 		return (print_error("an error has occured when creating a window\n", 1));
+	mlx_hook(fractol->mlx->win, 2, 1L<<0, close_mlx_win, fractol->mlx);
 	mlx_loop(fractol->mlx->mlx);
 	return (0);
 }
